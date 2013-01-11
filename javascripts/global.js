e024396120ebb476f3714f059dbf3eaa63ee22ef
dot-soft <dot-soft@mail.com>
2013-01-12 00:25:07 +0330

    eyval

--- javascripts/global.js
+++ javascripts/global.js
@@ -1,1 +1,49 @@
-$('#main ul li')
+jQuery(function($){
+
+    var colors={
+        meat: '#ba4b58',
+        bread: '#d5b146',
+        vegetables: '#5fbf35',
+        nuts: '#c06c12',
+        results: '#a52a2a'
+    };
+
+    $('#main').find('ul li').click(function(){
+        $(this).toggleClass('selected');
+    });
+
+
+    $('#main-steps').find('a').click(function(){
+
+        var step = this.id;
+
+        toggleActiveList(step);
+        changeBgColors(step);
+        fadeIngredients(step);
+
+    });
+
+
+    var changeBgColors = function(step) {
+        $('#main-steps').find('a').stop().css('background', '#333');
+
+        $('#main-steps').find('a#' + step).animate({
+            backgroundColor: colors[step]
+        }, 1000);
+        $('body').animate({
+           backgroundColor: colors[step]
+        }, 1000);
+    }
+
+    var toggleActiveList = function(step){
+        $('#main').find('ul li').addClass('opacity-zero');
+        $('#main').find('ul.active').removeClass();
+        $('#main').find('ul#' + step + '-list').addClass('active');
+    }
+
+    var fadeIngredients = function(step){
+        $('#main').find('ul#' + step + '-list li').fadeTo("slow", 1);
+
+    }
+
+});
